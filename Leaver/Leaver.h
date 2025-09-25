bb85c6efbedd0cac00e44f8e42a9430834b44d52
artem <artem.gaponov.2024@mail.ru>
2025-09-25 20:01:23 +0300

    first

---
+++ Leaver/Leaver.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+#ifdef LEAVER_EXPORT
+#define LEAVER_API __declspec(dllexport)
+#else
+#define LEAVER_API __declspec(dllimport)
+#endif
+class Leaver
+{
+public:
+	LEAVER_API std::string leave(std::string);
+};
